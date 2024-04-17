c0388f904afc99096d6edabde7e77032aa4b0ecd
SamJ51 <samueleleveld@gmail.com>
2024-04-17 12:47:59 +1000

    Fixed read content button so that it works first try.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 function readContent(postNumber) {
-    const content = document.getElementById('post'+postNumber)
-    const readMore = document.getElementById('read-button'+postNumber)
+    const content = document.getElementById('post' + postNumber);
+    const readMore = document.getElementById('read-button' + postNumber);
 
-    if (content.style.display === 'none') {
-        content.style.display = 'block'
-        readMore.textContent = 'Hide Content...'
-    }
-    else
-    {
-        content.style.display = 'none'
-        readMore.textContent = 'Read Content...'
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+        readMore.textContent = 'Read Content...';
+    } else {
+        content.style.display = 'block';
+        readMore.textContent = 'Hide Content...';
     }
 }
